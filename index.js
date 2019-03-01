a18431aa7042d1e626b21ad7f315a35efe920804
esteban zambrano <ezam0000@gmail.com>
2019-03-01 00:09:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ function happyHolidaysTo(string) {
 }
 
 name = 'you'
-var holiday = 'Independence Day'
+var holiday0 = 'Independence Day'
 
 function happyHolidayTo (string) {
   return `Happy ${holiday}, ${name}!`
 }
 var days=20
-
+var holiday1 = 
 function holidayCountdown (string){
   return `It's ${days} days until ${holiday}!`
 }
